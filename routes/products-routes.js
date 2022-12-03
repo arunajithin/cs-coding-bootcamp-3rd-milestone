f209282acf47fa8f68694d38d5274e6a799031e6
arunajithin <116826164+arunajithin@users.noreply.github.com>
2022-12-03 23:39:01 +0400

    product listing route and user update route

---
+++ routes/products-routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const ProductModel = require('../models/ProductModel');
+
+// products/add
+router.post('/create',
+    function(req, res) {
+        
+    }
+);
+
+// products/listing
+router.post('/list',
+    function(req, res) {
+        ProductModel
+        .find(req.query)
+        .then(
+            function(dbDocument){
+                res.json(dbDocument)
+            }
+        )
+        .catch(
+            function(error){
+                console.log('products/list error', error);
+
+                res.send('An error occured');
+            }
+        )
+    }
+);
+
+// products/update
+router.put('/update',
+    
+);
+
+module.exports = router;
